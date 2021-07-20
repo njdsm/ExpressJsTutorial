d48130769b20b7760819884e38f25575c8685eda
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-07-20 10:31:38 -0500

    working through tutorial, added navigation components for mobile and desktop views.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/newPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   )
 }
