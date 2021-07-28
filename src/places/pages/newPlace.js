a710f9ba7c3a37aeaa3bef66ff43f9ea4b83fecb
njdsm <nathanial.r.johnson@gmail.com>
2021-07-28 15:06:45 -0500

    basic validation implemented

--- src/places/pages/newPlace.js
+++ src/places/pages/newPlace.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import './newPlace.css';
 
 const NewPlace = () => {
     return <form className="place-form">
-        <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid title."></Input>
+        <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title."></Input>
     </form>
 };
 
